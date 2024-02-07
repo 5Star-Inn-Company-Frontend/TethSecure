7fd66ec99c6949b5012f150eb0d7229500aa66ec
Soares Ayoigbala <soaresayoigbala@gmail.com>
2024-02-07 05:36:40 +0100

    Another

--- lib/pages/security_screen.dart
+++ lib/pages/security_screen.dart
@@ -1,3 +1,4 @@
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tethsecure/components/drawer.dart';
@@ -10,6 +11,8 @@ class SecurityScreen extends StatefulWidget {
 }
 
 class _SecurityScreenState extends State<SecurityScreen> {
+  final int _duration = 25;
+
   // Seurity code for user
   String securityCode = "7938-9081";
 
@@ -42,66 +45,100 @@ class _SecurityScreenState extends State<SecurityScreen> {
       drawer: const DrawerComp(),
       backgroundColor: const Color.fromRGBO(0, 8, 35, 1),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 60,),
-              Center(
-                child: SizedBox(
-                  width: 300,
-                  child: Table(
-                    border: TableBorder.all(width:2, color: Colors.white), //table border
-                    children:  [
-                      const TableRow(
-                        children: [
-                          TableCell(
-                            child: Padding(
-                              padding: EdgeInsets.symmetric(vertical: 5),
-                              child: Text("Security Code", textAlign: TextAlign.center, style: TextStyle(fontSize: 16 , fontWeight: FontWeight.w500, color: Colors.white),),
-                            )
-                          ),
-                        ]
-                      ),
-                      TableRow(
-                        children: [
-                          TableCell(
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 8, right: 8, top: 8, bottom: 8),
-                              child:Text(securityCode, textAlign: TextAlign.center, style: const TextStyle(fontSize: 55, fontWeight: FontWeight.w700, color: Colors.white),)
-                            )
+        child: Column(
+          children: [
+            const SizedBox(height: 60,),
+            Center(
+              child: SizedBox(
+                width: 300,
+                child: Table(
+                  border: TableBorder.all(width:2, color: Colors.white), //table border
+                  children:  [
+                    const TableRow(
+                      children: [
+                        TableCell(
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(vertical: 5),
+                            child: Text("Security Code", textAlign: TextAlign.center, style: TextStyle(fontSize: 16 , fontWeight: FontWeight.w500, color: Colors.white),),
                           )
-                        ]
-                      ),
-                    ],
-                  ),
+                        ),
+                      ]
+                    ),
+                    TableRow(
+                      children: [
+                        TableCell(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 8, right: 8, top: 8, bottom: 8),
+                            child:Text(securityCode, textAlign: TextAlign.center, style: const TextStyle(fontSize: 55, fontWeight: FontWeight.w700, color: Colors.white),)
+                          )
+                        )
+                      ]
+                    ),
+                  ],
                 ),
               ),
-
-              const SizedBox(height: 100,),
-              // const SizedBox(
-              //   height: 100,
-              //   width: 100,
-              //   child: CircularProgressIndicator(
-              //     strokeWidth: 15,
-              //   ),
-              // ),
-              const Text("25", textAlign: TextAlign.center, style: TextStyle(fontSize: 70 , fontWeight: FontWeight.w500, color: Colors.white),),
-
-              // const SizedBox(height: 5,),
-              const Text("LIFTTIME", textAlign: TextAlign.center, style: TextStyle(fontSize: 16 , fontWeight: FontWeight.w500, color: Colors.white),),
-
-              const SizedBox(height: 50,),
-              const Text("Serial Number: 0227489994", textAlign: TextAlign.center, style: TextStyle(fontSize: 16 , fontWeight: FontWeight.w500, color: Colors.white),),
-
-              const SizedBox(height: 180,),
-              const Row(
-                children: [
-                  SizedBox(width: 280,),
-                  Text("TETHSECURE", style: TextStyle(fontSize: 16 , fontWeight: FontWeight.w500, color: Colors.white),),
-                ],
-              )
-            ],
-          )
+            ),
+        
+            const SizedBox(height: 90,),
+            CircularCountDownTimer(
+              timeFormatterFunction: (defaultFormatterFunction, duration) {
+                if (duration.inSeconds == 0) {
+                  return "0";
+                } else {
+                  return Function.apply(defaultFormatterFunction, [duration]);
+                }
+              },
+              // Countdown duration in Seconds.
+              duration: _duration,
+              // Countdown initial elapsed Duration in Seconds.
+              initialDuration: 0,
+              width: 121,
+              // Height of the Countdown Widget.
+              height: 121,
+              // Ring Color for Countdown Widget.
+              ringColor: const Color.fromRGBO(1, 28, 122, 1),
+              // Ring Gradient for Countdown Widget.
+              ringGradient: null,
+              // Filling Color for Countdown Widget.
+              fillColor: Colors.white,
+              // Filling Gradient for Countdown Widget.
+              fillGradient: null,
+              // Background Color for Countdown Widget.
+              backgroundColor: const Color.fromRGBO(0, 0, 0, 1),
+              // Background Gradient for Countdown Widget.
+              backgroundGradient: null,
+              // Border Thickness of the Countdown Ring.
+              strokeWidth: 20.0,
+              // Begin and end contours with a flat edge and no extension.
+              strokeCap: StrokeCap.square,
+              // Text Style for Countdown Text.
+              textStyle: const TextStyle(fontSize: 47, fontWeight: FontWeight.w500, color: Colors.white),
+              // Format for the Countdown Text.
+              textFormat: CountdownTextFormat.S,
+              // Handles Countdown Timer (true for Reverse Countdown (max to 0), false for Forward Countdown (0 to max)).
+              isReverse: true,
+              // Handles Animation Direction (true for Reverse Animation, false for Forward Animation).
+              isReverseAnimation: true,
+              // Handles visibility of the Countdown Text.
+              isTimerTextShown: true,
+              // Handles the timer start.
+              autoStart: true,
+            ),
+        
+            const SizedBox(height: 15,),
+            const Text("LIFTTIME", textAlign: TextAlign.center, style: TextStyle(fontSize: 16 , fontWeight: FontWeight.w500, color: Colors.white),),
+        
+            const SizedBox(height: 50,),
+            const Text("Serial Number: 0227489994", textAlign: TextAlign.center, style: TextStyle(fontSize: 16 , fontWeight: FontWeight.w500, color: Colors.white),),
+        
+            const SizedBox(height: 150,),
+            const Row(
+              children: [
+                SizedBox(width: 280,),
+                Text("TETHSECURE", style: TextStyle(fontSize: 16 , fontWeight: FontWeight.w500, color: Colors.white),),
+              ],
+            )
+          ],
         ),
       ),
     );
